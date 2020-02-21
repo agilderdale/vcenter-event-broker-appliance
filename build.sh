6ece47ef8313111d13a941e925b4369c6d404c8a
agilderdale <agilderdale@vmware.com>
2020-02-21 10:50:17 +0000

    updated build script to use docker image for packer
    Signed-off-by: agilderdale <agilderdale@vmware.com>

--- build.sh
+++ build.sh
@@ -7,8 +7,10 @@ rm -f output-vmware-iso/*.ova
 
 if [[ "$1" -gt "-1" ]] && [[ $1 == "dev" ]]; then
     echo "Applying packer build to photon-dev.json ..."
-    packer build -var-file=photon-builder.json -var-file=photon-version.json photon-dev.json
+#    packer build -var-file=photon-builder.json -var-file=photon-version.json photon-dev.json
+    docker run -i -t hashicorp/packer:full build -var-file=photon-builder.json -var-file=photon-version.json photon-dev.json
 else
     echo "Applying packer build to photon.json ..."
-    packer build -var-file=photon-builder.json -var-file=photon-version.json photon.json
+#    packer build -var-file=photon-builder.json -var-file=photon-version.json photon.json
+    docker run -i -t hashicorp/packer:full build -var-file=photon-builder.json -var-file=photon-version.json photon.json
 fi
